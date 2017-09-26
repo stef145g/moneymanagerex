8ad2e124f1c3fb9897318471df59d3c26c0d15e0
Stefano <stef145g@gmail.com>
2017-09-27 02:10:59 +1000

    feat(MSCV.Test.Build): update configurations

---
+++ src/versions.h
@@ -0,0 +1,28 @@
+//----------------------------------------------------------------------------
+// WARNING: The file versions.h is produced by CMake. If you want to make
+//          some changes in this file please use the templated version
+//          versions.h.in and rerun cmake to produce a refreshed
+//          version of versions.h
+//
+// Note:    MMEX version changes are made in the main CMakeLists.txt
+//----------------------------------------------------------------------------
+
+#define MMEX_VERSION_MAJOR 1
+#define MMEX_VERSION_MINOR 4
+#define MMEX_VERSION_PATCH 0
+#define MMEX_VERSION_ALPHA 3
+#define MMEX_VERSION_BETA  -1
+#define MMEX_VERSION_RC    -1
+
+#define GIT_COMMIT_HASH "b8faffc"
+#define GIT_COMMIT_DATE "2017-09-14"
+#define GIT_BRANCH      "master"
+
+#define CMAKE_VERSION   "CMake 3.9.1"
+#define CMAKE_GENERATOR "Visual Studio 14 2015 Win64"
+#define MAKE_VERSION    "MSBuild 14.0.25420.1"
+#define GETTEXT_VERSION "gettext 0.19.8.1"
+#define CXX_VERSION     "19.0.24215.1"
+/* #undef VS_VERSION */
+/* #undef CMAKE_VS_WINDOWS_TARGET_PLATFORM_VERSION */
+/* #undef LINUX_DISTRO_STRING */
